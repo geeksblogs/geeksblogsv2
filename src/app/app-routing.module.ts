538c1526fb3c856922293d10991e8d404f819987
Harshita Ramchandani <harshitaramchandani123@gmail.com>
2020-04-26 23:12:04 +0530

    commit 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './course/course.component';
 import { ContactComponent } from './contact/contact.component';
+// import { NavbarComponent } from './header/navbar/navbar.component';
+// import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'courses', component: CourseComponent },
   { path: 'contact', component: ContactComponent },
+  // {path : 'navbar', component: NavbarComponent},
+  // {
+  //   path: 'footer', component: FooterComponent
+  // }
 
 ];
 
