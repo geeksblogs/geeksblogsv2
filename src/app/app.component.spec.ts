6861954d075159a2ab00c0ae129b8d3d77280e2e
Harshita Ramchandani <harshitaramchandani123@gmail.com>
2020-04-27 15:44:42 +0530

    hey

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('');
+    expect(compiled.querySelector('.content span').textContent).toContain('GeeksBlogs app is running!');
   });
 });
